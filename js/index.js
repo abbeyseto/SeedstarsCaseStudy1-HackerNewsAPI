f633b43b4f35e430c9594f8f7cea3ab0d62fa229
abbeyseto <adenleabbey@hotmail.com>
2018-07-28 01:40:10 +0100

    infinite scroll on PC and mobile screen added, comment also added to index.js file

--- js/index.js
+++ js/index.js
@@ -1,82 +1,57 @@
-/* eslint-env jquery */
-let page = 0;
-let count = 0;
-let newCount = 0;
-(function () {
-    $('.loader').hide();
-    const form = document.querySelector('#search-form');
-    const searchField = document.querySelector('#search-keyword');
-    let searchedForText;
-    const responseContainer = document.querySelector('#response-container');
+let page = 0;  //page is set to Zero
+let count = 0; // count is set to Zer0
 
-    form.addEventListener('submit', function (e) {
-        e.preventDefault();
+(function () { // On windows load this function is called.
+    $('.loader').hide(); // div used as spinning loader icon is hidden on page load
+    const form = document.querySelector('#search-form'); // form element used for the search input is saved as 'form'
+    const searchField = document.querySelector('#search-keyword'); // input field element that will contain the search keyword is saved as 'searchField'
+    let searchedForText; //a variable is declared
+    const responseContainer = document.querySelector('#response-container'); // div where results from the ajax call will be displayed is declared
+
+    form.addEventListener('submit', function (e) { // click event on the submit button calls this function
+        page = 0; // page number is reset back to zero
+        count = 0; // count number is reset back to zero
+        e.preventDefault(); // click buton's default action is blocked
         responseContainer.innerHTML = '';
-        searchedForText = searchField.value;
-        getData();
-        /* $.ajax({
-             url: `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=61d4085b01144e2194ac5866d2c36210`,
-         }).done(addArticles)
-             .fail(function (err) {
-                 requestError(err, 'articles');
-             });*/
+        searchedForText = searchField.value; // input value which is the search keyword is saved in the variable declared at line 8
+        getData(); // function containing the ajax call (starting at line 20) is called when button is clicked
     });
 
-    function getData() {
-        ++page;
-        var url = 'https://newsapi.org/v2/everything?sources=hacker-news&q=' + `${searchedForText}` + '&apiKey=816734caecd74eb08f84b8c38cf27f6c&page=' + page;
-        setTimeout(function () {
-            $.ajax({
+    function getData() { //function containing the ajax
+        ++page; // page is increased by 1
+        var url = 'https://newsapi.org/v2/everything?sources=hacker-news&q=' + `${searchedForText}` + '&apiKey=816734caecd74eb08f84b8c38cf27f6c&page=' + page; //keyword value from  line 16 and increased page number from line 21 is added to the ajax url string.
+            $.ajax({        // Ajax request call is made to HackerNews REST API
                 url: url,
                 method: 'GET',
-            }).done(addArticles).fail(function (err) {
-                throw err;
-            });
-        }, 500);
-    }
-
-    function addArticles(data) {
-        let htmlContent = '';
-
-        if (data && data.articles && data.articles.length > 1) {
-            const results = data.articles;
-
-            htmlContent = '<ul>' + results.map(news => `<li class="article"> <h2><a href="${news.url}">${news.title}</a></h2><sub>Published on: ${(new Date(news.publishedAt)).toUTCString()}</sub><br><p>${news.description}</p>
-            <p align="right">Source: ${news.source.name} </p></li>`
-            ).join('') + '</ul>';
-            count++;
-        } else {
-            htmlContent = `<div class="error-no-articles">No articles available</div>`;
+            }).done(addArticles).fail(requestError);
         }
-        responseContainer.insertAdjacentHTML('beforeend', '<h3>Page: <span  id="count">' + count + '</Span></h3>');
-        responseContainer.insertAdjacentHTML('beforeend', htmlContent);
+
+    function addArticles(data) { // function that manipulates response fron Ajax call
+        let htmlContent = '';  // varable declared and value saved as empty string
+
+        if (data && data.articles && data.articles.length > 1) { // if data is recieved from the ajax call and the response is more than 1
+            const results = data.articles; // save the data array in a variable 'results'
+
+            htmlContent = '<ul>' + results.map(news => `<li class="article"> <h2><a href="${news.url}">${news.title}</a></h2><sub>Published on: ${(new Date(news.publishedAt)).toUTCString()}</sub><br><p>${news.description}</p><p align="right">Source: ${news.source.name} </p></li>`).join('') + '</ul>'; // Ajax response maps 'results' and it is formatted to a readable thumbnail format and stored in the variable 'htmlContent'.
+            count++; // count is increased each time formated response is captured and saved in the variable 'htmlContent'
+        } else { // else if no more data is recieved
+            htmlContent = `<div class="error-no-articles">No more articles available</div>`; // error message is saved in the variable 'htmlContent'
+        }
+        responseContainer.insertAdjacentHTML('beforeend', '<h3>Page: <span  id="count">' + count + '</Span></h3>'); //count is added to the DOM as the result's page number
+        responseContainer.insertAdjacentHTML('beforeend', htmlContent); // formated ajax response from line 37 is added to the DOM
     }
-    function requestError(e, part) {
+    function requestError(e, part) { //function that handles error when there is no response from the REST API
         console.log(e);
-        responseContainer.insertAdjacentHTML('beforeend', `<p class="network-warning error-${part}">Oh no! There was an error making a request for the ${part}.</p>`);
+        responseContainer.insertAdjacentHTML('beforeend', `<p class="network-warning error-${part}">Oh no! There was an error making a request from HackerNews,  Please check you internet connection.</p>`);
     }
 
-    /*$(window).scroll(function () {
-        //$('.loader').delay(5000).fadeIn(500);
-        console.log(window.scrollY);
-        console.log((document.getElementById('main').scrollHeight));
-        if ((document.getElementById('main').scrollHeight)  === window.scrollY ) {
-            $('.loader').delay(5000).fadeIn(500);
-           // var new_div = '<div class="new_block"><h2>Page ' + page + '</h2></div>';
-            //$('#response-container').delay(5000).append(new_div);
-            $('#response-container').append(getData());
-    }
-});
-*/
-
-    $(window).scroll(function () {
+    $(window).scroll(function () { // funstion that handles "Infinite Scroll"
         console.log(count);
         console.log(page);
-        if ($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
-            $('.loader').delay(1000).fadeIn(500);
-            if (count === page) {
-                $('#response-container').append(getData());
-                //$(window).unbind('scroll');
+        if ($(window).scrollTop() + $(window).height() > $(document).height() - 100) {// checks is the user has scrolled to the bottom of the page
+            $('.loader').delay(1000).fadeIn(500); //spinning loader is displayed below the page
+            if (count === page) { // checks is the page number is the same as the number of request that has been made so far from the HackerNews REST API
+                $('#response-container').append(getData());// if yes, another Ajax request is made again and the formatted result is appended to the DOM and user can continue scrolling
             }
         }
     });
